9b4a472dbbc4846db6986db3d038386772616883
Medina, Ivan Angel <ivanangelmedina@gmail.com>
2024-08-25 16:51:25 -0300

    Primer commit para probar que la conexion con github funcione correctamente.

---
+++ src/ar/edu/unq/probando/Probando.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.probando;
+
+public class Probando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World");
+		
+		
+		
+
+	}
+
+}
